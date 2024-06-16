aa068b260a0e0225dee893d9b05d7ac650fcbe11
bgropay <bgropay@gmail.com>
2024-06-17 01:16:58 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 #
 # PyUnzip
+# =======
 #
-# Program untuk meng-crack kata sandi file Zip
+# PyUnzip adalah program Python sederhana 
+# yang dirancang untuk meng-crack kata sandi
+# file Zip. Program ini menggunakan teknik
+# Dictionary Attack, yaitu teknik yang mencoba
+# semua kemungkinan kata sandi yang terdapat
+# dalam file Wordlist.
 #
-# Jenis Enkripsi yang didukung PyUnzip:
+#
+# Jenis Enkripsi yang didukung PyUnzip
+#-------------------------------------
+#
 # [+] ZipCrypto
 # [+] AES-128
 # [+] AES-192
 # [+] AES-256
 #
-# Dibuat oleh BgRopay
+#
+# Pembuat 
+# -------
+#
+# BgRopay
 
 import os
 import time
