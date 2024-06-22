23f10b637e6df15c3c8ba2e8123e45040c3c7efd
bgropay <bgropay@gmail.com>
2024-06-22 19:04:40 +0700

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,9 +1,10 @@
 print("Program berjalan... Tekan Ctrl+C atau Ctrl+Z untuk mencoba menghentikan.")
 
-try:
-    while True:
-        pass  # Program utama berjalan di sini
-except KeyboardInterrupt:
-    print("Mengabaikan Ctrl+C!")
-except SystemExit:
-    print("Mengabaikan Ctrl+Z!")
+while True:
+    try:
+        while True:
+             pass  # Program utama berjalan di sini
+    except KeyboardInterrupt:
+        print("Mengabaikan Ctrl+C!")
+    except SystemExit:
+        print("Mengabaikan Ctrl+Z!")
