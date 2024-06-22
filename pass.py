e0052b782431ba10344955e6664c4d168c5045ee
bgropay <bgropay@gmail.com>
2024-06-22 19:01:39 +0700

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,23 +1,9 @@
-import signal
-import sys
-
-# Handler untuk SIGINT (Ctrl+C)
-def signal_handler(sig, frame):
-    print('Mengabaikan Ctrl+C!')
-
-# Handler untuk SIGTSTP (Ctrl+Z)
-def signal_handler_tstp(sig, frame):
-    print('Mengabaikan Ctrl+Z!')
-
-# Tangkap sinyal SIGINT dan SIGTSTP
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTSTP, signal_handler_tstp)
-
 print("Program berjalan... Tekan Ctrl+C atau Ctrl+Z untuk mencoba menghentikan.")
 
-# Program loop untuk mempertahankan script tetap berjalan
-while True:
-    try:
-        pass  # Lakukan sesuatu di sini
-    except KeyboardInterrupt:
-        pass  # Mengabaikan Ctrl+C
+try:
+    while True:
+        pass  # Program utama berjalan di sini
+except KeyboardInterrupt:
+    print("Mengabaikan Ctrl+C!")
+except SystemExit:
+    print("Mengabaikan Ctrl+Z!")
