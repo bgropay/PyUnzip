d2876b386f2c3555e33314009fe13efb41a83d3b
bgropay <bgropay@gmail.com>
2024-07-14 22:46:32 +0700

    Update v2.py

--- v2.py
+++ v2.py
@@ -286,7 +286,7 @@ elif metode_serangan == "2":
                 elif rules_dic == "4" or rules_dic == daftar_rules_dic[3]:
                     kombinasirules_dic.append(string.punctuation)
                 else:
-                    print(f"{m}[-] {p} Kombinasi karakter '{rules_dic}' tidak tersedia.{r}")
+                    print(f"{m}[-] {p}Kombinasi karakter '{rules_dic}' tidak tersedia.{r}")
                 counter_dic += 1
             except KeyboardInterrupt:
                 print(f"\n{m}[-] {p}Keluar...{k}:({r}")
